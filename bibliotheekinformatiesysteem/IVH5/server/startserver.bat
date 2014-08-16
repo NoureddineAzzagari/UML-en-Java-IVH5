aa1e22db2a62f0bc3fa8670e86e02dd4ef087fb5
Robin Schellius <r.schellius@avans.nl>
2014-08-16 13:38:34 +0200

    Kleine verbeteringen.

--- bibliotheekinformatiesysteem/IVH5/server/startserver.bat
+++ bibliotheekinformatiesysteem/IVH5/server/startserver.bat
@@ -1,5 +1,5 @@
 ::
-:: Batch bestand voor het starten van de IVH5 LibraryServer.
+:: Batchbestand voor het starten van de IVH5 LibraryServer.
 :: Je start de server het handigst vanuit de directory waar de webserver
 :: de classes kan vinden. Pas deze zo nodig hieronder aan.
 ::
@@ -7,9 +7,9 @@
 cd \dev\xampp\htdocs\classes
 
 :: Start java met het juiste classpath
-java -cp .;log4j-1.2.17.jar;mysql-connector-java-5.1.31.jar library.main.LibraryServer -servicename BiebBreda
-
-:: Wanneer je securityproblement wilt oplossen, voeg dan onderstaade optie aan het command toe.
+java -cp .;log4j-1.2.17.jar;mysql-connector-java-5.1.31.jar library.main.LibraryServer -servicename "BibliotheekBreda"
+ 
+:: Wanneer je securityproblemen wilt oplossen, voeg dan onderstaade optie aan het command toe.
 :: Hiermee krijg je inzicht in alle security instellingen.
 ::
 :: 		-Djava.security.debug=access,failure
