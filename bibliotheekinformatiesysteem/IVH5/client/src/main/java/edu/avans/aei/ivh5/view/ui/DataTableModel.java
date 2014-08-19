27427bda3000fa6efcffd154cd0b52b5a168eaea
Robin Schellius <r.schellius@avans.nl>
2014-08-19 14:57:17 +0200

    Updated comments, finished formatting.

--- bibliotheekinformatiesysteem/IVH5/client/src/main/java/edu/avans/aei/ivh5/view/ui/DataTableModel.java
+++ bibliotheekinformatiesysteem/IVH5/client/src/main/java/edu/avans/aei/ivh5/view/ui/DataTableModel.java
@@ -8,63 +8,64 @@ import org.apache.log4j.Logger;
 
 /**
  * This class provides a data model for the JTable component in the UI. Using this data model
- * enables us to quickly perform operations on the table containing member information. 
+ * enables us to quickly perform operations on the table containing member information. The contents of 
+ * this data model are automatically displayed in the corresponding JTable. 
  * 
  * @author Robin Schellius
- *
  */
 @SuppressWarnings("serial")
 public class DataTableModel extends AbstractTableModel {
 
-	Vector<String[]> cache; // will hold String[] objects . . .
-	int colCount;
-	String[] headers;
+	private Vector<String[]> cache; // will hold String[] objects . . .
+	private int colCount;
+	private String[] headers;
 
 	// Get a logger instance for the current class
 	static Logger logger = Logger.getLogger(DataTableModel.class);
 
 	/**
-	 * 
+	 * Constructor.
 	 */
 	public DataTableModel() {
 		logger.debug("Constructor");
 
-		cache = new Vector<String[]>();
 		colCount = 4;
 		headers = new String[colCount];
+		cache = new Vector<String[]>();
 	}
 
 	/**
-	 * 
+	 * Get the name of column [i].
 	 */
 	public String getColumnName(int i) { 
 		return headers[i];
 	}
 
 	/**
-	 * 
+	 * Get the number of columns in the model.
 	 */
 	public int getColumnCount() {
 		return colCount;
 	}
 
 	/**
-	 * 
+	 * Get the number of rows.
 	 */
 	public int getRowCount() {
 		return cache.size();
 	}
 
 	/**
-	 * 
+	 * Get the value at [row,col].
 	 */
 	public Object getValueAt(int row, int col) {
 		return ((String[]) cache.elementAt(row))[col];
 	}
 
 	/**
+	 * Set the header row of the table. The header row displays the column names in the table.
 	 * 
-	 * @param header
+	 * @param header Array of strings containing the column names.
 	 */
 	public void setTableHeader(String[] header) {
 		logger.debug("setTableHeader");
@@ -73,31 +74,32 @@ public class DataTableModel extends AbstractTableModel {
 		for (int h = 0; h < colCount; h++) {
 			headers[h] = header[h];
 		}
-
 	}
 
 	/**
+	 * Set the values of the data table model.
 	 * 
-	 * @param rs
+	 * @param values String[][] containing the values to be set.
 	 */
-	public void setValues(String[][] rs) {
+	public void setValues(String[][] values) {
 		logger.debug("setValues");
 
 		cache = new Vector<String[]>();
 		try {
 			int i = 0;
-			while (i < rs.length) {
-				String[] record = new String[colCount];
+			while (i < values.length) {
+				String[] row = new String[colCount];
 				for (int j = 0; j < colCount; j++) {
-					record[j] = rs[i][j];
+					row[j] = values[i][j];
 				}
-				cache.addElement(record);
+				cache.addElement(row);
 				i++;
 			}
-			fireTableChanged(null); // notify everyone that we have a new table.
+			// notify everyone that we have a new table.
+			fireTableChanged(null); 
 		} catch (Exception e) {
-			cache = new Vector<String[]>(); // blank it out and keep going.
-			e.printStackTrace();
+			cache = new Vector<String[]>();
+			logger.error(e.getMessage());
 		}
 	}
 }
